ce012223fe748248fc5892eeefad063576086092
Damilare <dre4success@live.com>
2018-10-31 23:35:35 +0100

    selecting input fields from google calendar

---
+++ fireflies.js
@@ -0,0 +1,20 @@
+(function() {
+  let id = 'aksdfas';
+
+  // create button and style it
+  let button = document.createElement('button');
+  button.style.backgroundColor = '#ff0092';
+  button.style.borderRadius = '4px';
+  button.style.padding = '7px';
+  button.style.marginLeft = '200px';
+  let text = document.createTextNode('Transcribe with Fireflies.ai');
+  button.appendChild(text);
+
+  // get the div to place the button in
+  let parentDiv = document.querySelector('.emaTS');
+  parentDiv.appendChild(button);
+
+  let description = document.getElementById('hInySc0');
+  let location = document.querySelector('[aria-label="Location"]');
+  let guest = document.querySelector('[aria-owns="nngdp149"]');
+})();
