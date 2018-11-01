ec0b85f927b31b294f13e2253af8fdaaf64a5ea6
Damilare <dre4success@live.com>
2018-11-01 21:46:28 +0100

    loading script when you get to eventedit

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status !== 'complete') return;
 
-  let match = 'https://calender.google.com/calender/r'
+  let match = 'https://calendar.google.com/calendar/r/eventedit';
   if (tab.url.substring(0, match.length) === match) {
     chrome.tabs.executeScript(tabId, { file: 'fireflies.js' });
   }
